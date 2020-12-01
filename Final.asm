9fc0f539b6a1834b011c2c489f9e0a142267db63
Mati Dastugue <mdastugu@amazon.com>
2020-12-01 14:02:54 -0300

    Armado de zip final

--- Final.asm
+++ Final.asm
@@ -12,9 +12,16 @@ String0 db "Ingrese un valor pivot mayor o igual a 1: ", '$'
 pivot dd ?
 _1 dd 1
 @pos dd ?
+_10 dd 10
 _0 dd 0
+_20 dd 20
 _2 dd 2
+_30 dd 30
 _3 dd 3
+_40 dd 40
+_4 dd 4
+_5 dd 5
+_6 dd 6
 resul dd ?
 String1 db "Elemento encontrado en posicion: ", '$'
 String2 db "El valor debe ser >=1", '$'
@@ -37,11 +44,11 @@ FXCH
 FCOMP
 FSTSW AX
 SAHF
-JB etiqueta285
+JB etiqueta303
 FILD _0
 FISTP @pos
 FILD pivot
-FILD _1
+FILD _10
 FXCH
 FCOMP
 FSTSW AX
@@ -58,7 +65,7 @@ FILD _1
 FISTP @pos
 etiqueta266:
 FILD pivot
-FILD _2
+FILD _20
 FXCH
 FCOMP
 FSTSW AX
@@ -75,7 +82,7 @@ FILD _2
 FISTP @pos
 etiqueta272:
 FILD pivot
-FILD _3
+FILD _30
 FXCH
 FCOMP
 FSTSW AX
@@ -91,13 +98,64 @@ JNE etiqueta278
 FILD _3
 FISTP @pos
 etiqueta278:
+FILD pivot
+FILD _40
+FXCH
+FCOMP
+FSTSW AX
+SAHF
+JNE etiqueta284
 FILD @pos
 FILD _0
 FXCH
 FCOMP
 FSTSW AX
 SAHF
-JE etiqueta288
+JNE etiqueta284
+FILD _4
+FISTP @pos
+etiqueta284:
+FILD pivot
+FILD _5
+FXCH
+FCOMP
+FSTSW AX
+SAHF
+JNE etiqueta290
+FILD @pos
+FILD _0
+FXCH
+FCOMP
+FSTSW AX
+SAHF
+JNE etiqueta290
+FILD _5
+FISTP @pos
+etiqueta290:
+FILD pivot
+FILD _4
+FXCH
+FCOMP
+FSTSW AX
+SAHF
+JNE etiqueta296
+FILD @pos
+FILD _0
+FXCH
+FCOMP
+FSTSW AX
+SAHF
+JNE etiqueta296
+FILD _6
+FISTP @pos
+etiqueta296:
+FILD @pos
+FILD _0
+FXCH
+FCOMP
+FSTSW AX
+SAHF
+JE etiqueta306
 FILD @pos
 FISTP resul
 displayString String1
@@ -106,17 +164,17 @@ displayString NEW_LINE
 DisplayInteger resul
 displayString NEW_LINE
 
-JMP etiqueta290
-etiqueta285:
+JMP etiqueta308
+etiqueta303:
 displayString String2
 displayString NEW_LINE
 
-JMP etiqueta290
-etiqueta288:
+JMP etiqueta308
+etiqueta306:
 displayString String3
 displayString NEW_LINE
 
-etiqueta290:
+etiqueta308:
 
 MOV AH, 1
 INT 21h
